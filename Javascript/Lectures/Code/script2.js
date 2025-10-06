36158007024eac79843e419259c45cbfb40400ff
Rawat890 <rohanrawat7508@gmail.com>
2025-10-06 11:53:47 +0530

    chore: script updated

--- Javascript/Lectures/Code/script2.js
+++ Javascript/Lectures/Code/script2.js
@@ -4,8 +4,8 @@ const obj = {
   age: 24,
   account_balance: 190,
   'account number': 38239032,
-  null:78,
-  undefined:10,
+  null: 78,
+  undefined: 10,
 }
 
 console.log(obj)
@@ -15,7 +15,7 @@ console.log(obj["account number"])
 delete obj["account number"];
 console.log(obj)
 
-const arr =[1,2,3,5,6];
+const arr = [1, 2, 3, 5, 6];
 delete arr[0];
 delete arr[3];
 console.log(arr)
@@ -23,12 +23,12 @@ console.log(arr)
 class People {
   constructor(name, age, gender) {
     this.name = name,
-    this.age = age,
-    this.gender= gender
+      this.age = age,
+      this.gender = gender
   }
 }
 
-let person1  = new People("Rohan",34, "Male");
+let person1 = new People("Rohan", 34, "Male");
 console.log(person1)
 
 console.log(Object.keys(obj).length)
@@ -47,10 +47,10 @@ console.log(obj2)
 
 //Shallow copy vs deep copy
 let employee = {
-    eid: "E102",
-    ename: "Jack",
-    eaddress: "New York",
-    salary: 50000
+  eid: "E102",
+  ename: "Jack",
+  eaddress: "New York",
+  salary: 50000
 }
 
 console.log("Employee=> ", employee);
@@ -65,10 +65,10 @@ console.log("New Employee=> ", newEmployee);
 // newEmployee is changed.
 
 let employee1 = {
-    eid: "E102",
-    ename: "Jack",
-    eaddress: "New York",
-    salary: 50000
+  eid: "E102",
+  ename: "Jack",
+  eaddress: "New York",
+  salary: 50000
 }
 console.log("=========Deep Copy========");
 let newEmployee1 = JSON.parse(JSON.stringify(employee1));
@@ -79,3 +79,33 @@ newEmployee1.ename = "Beck";
 newEmployee1.salary = 70000;
 console.log("Employee=> ", employee1);
 console.log("New Employee=> ", newEmployee1);
+
+
+const object1 = {
+  name: 'object1',
+  age: 23,
+  address: {
+    city: 'Hsp',
+    pincode: 146001
+  }
+}
+
+const deepCopy1 = structuredClone(object1);
+deepCopy1.address.pincode = 199990
+console.log(deepCopy1)
+console.log(object1)
+
+const deepCopy2 = Object.assign({}, object1);
+deepCopy2.address.pincode = 76234
+console.log(deepCopy2)
+console.log(object1)
+
+//We cannot use name to access , use fullName
+const { name: fullName, ...restData } = object1;
+console.log(fullName)
+console.log(restData)
+
+const {pincode} = object1.address;
+const {address:{city}} = object1;
+console.log(pincode)
+console.log(city)
