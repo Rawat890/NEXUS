802d9c1f63d9293868a929dbbc6df6eb8a06f7b3
Rawat890 <rohanrawat7508@gmail.com>
2025-10-07 11:47:24 +0530

    chore: file updated

---
+++ Javascript/Lectures/Code/script4.js
@@ -0,0 +1,38 @@
+const arr = [1,3,4,21,4,5,6];
+
+//for of loop
+for (const element of arr) {
+  console.log(element)
+}
+
+const person = {
+  name:'Kumar',
+  age:34,
+  profession:'cricketer'
+}
+//for of trick to iterate the object properties , but use for in always
+for (const element of Object.values(person)) {
+ console.log(element) 
+}
+
+const callName = (greet) =>{
+  console.log('hello I am name')
+  greet('Rohan Rawat')
+}
+
+const greet = (name) =>{
+  console.log('Good Morning ! ', name)
+}
+
+//passing greet reference in callName 
+callName(greet)
+
+arr.forEach(function call(item) {
+  console.log('Item - ', item)
+})
+
+const callNum = (num) =>{
+  console.log('hello I am name')
+  greet(`Rohan Rawat - ${num}`)
+}
+arr.forEach(callNum)
--- Javascript/Lectures/Lecture14.rst
+++ Javascript/Lectures/Lecture14.rst
@@ -3,9 +3,12 @@ The optimization technique is used in order to access the data.
 
 Advanced Loops in javascript - 
 for in loop - specifically use to iterate the keys of the objects
+for of loop used to iterate over the arrays
+Do not use for of for objects as objects are not iterable, as we do not have information about next iteratable , Symbol.iterable is not defined for objects
 
 Object.keys only prints the keys that is of that particular objects, but for in also prints the inherited properties as well
 
 Whenever we create a key value paor in object, 3 properties added by default - enumerable, writable and configurable
 
 If configurable is true, then we can change boolena values of other 2 properties
+
