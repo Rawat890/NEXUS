2466743efb8c8363d0087b753d0f357edc1d2d74
Rawat890 <rohanrawat7508@gmail.com>
2025-10-15 11:33:51 +0530

    chore: screenshots added

--- Javascript/Code/script6.js
+++ Javascript/Code/script6.js
@@ -1,6 +1,6 @@
 //QUES 1. Find the sum of all elements in an array
 
-const arr = [1,34,12,'', 'Hero',45,19,33];
+const arr = [1,34,12,'', 'Hero',-45,19,33];
 
 function printSum1(arr) {
   let sum =0;
@@ -47,5 +47,111 @@ console.log('Solution 1 - ', printSum1(arr))
 
 //QUES 2. Find the largest number in array
 function largestNumber1(arr) {
-  
+  let maximum = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (typeof arr[i]==='number' && maximum < arr[i]) {
+      maximum =arr[i];
+    }
+  }
+  return maximum;
 }
+
+function largestNumber2(arr) {
+  let maximum = arr[0];
+  arr.forEach((element)=>{
+    if (typeof element==='number' && element > maximum) {
+      maximum=element
+    }
+  })
+  return maximum;
+}
+
+function largestNumber3(arr) {
+  return arr.reduce((max, current) => {
+    if (typeof current === 'number' && current > max) {
+      return current;
+    }
+    return max;
+  }, arr[0]);
+}
+
+console.log('maximum 1 - ', largestNumber1(arr))
+console.log('maximum 2 - ', largestNumber2(arr))
+console.log('maximum 3 - ', largestNumber3(arr))
+
+//QUES 3. Find the smallest number in an array
+function smallestNumber1(arr) {
+  return arr.reduce((min, current)=>{
+    if (typeof current==='number' && current < min) {
+      return current;
+    }
+    return min;
+  }, arr[0])
+}
+
+console.log('Smallest 1 - ', smallestNumber1(arr))
+
+//QUES 4. Reverse an array without using the built-in reverse method
+function arrayReverse1(arr) {
+  for (let i = 0, j =arr.length-1 ; i <=j; i++, j--) {
+    let temp = arr[i];
+    arr[i]=arr[j];
+    arr[j]=temp;
+  }
+
+  console.log('Reversed array - ', arr);
+}
+
+function arrayReverse2(arr) {  //modifies original array
+  const reversed = arr.reverse();
+  console.log(reversed)
+}
+
+function arrayReverse3(arr) {  
+  const reversed = [...arr].reverse();
+  console.log(reversed)
+}
+
+function arrayReverse4(arr) {  
+  const reversed = arr.reduceRight((acc, current) => {
+  acc.push(current);
+  return acc;
+  }, []);
+  console.log(reversed)
+}
+
+arrayReverse4(arr)
+
+
+//QUES 5. Calculate the average of numbers in an array
+function average1(arr) {
+  const sum = arr.reduce((acc, current)=>{
+    if (typeof current == 'number') {
+      acc = acc + current;
+    }
+    return acc;
+  }, 0);
+  return sum/arr.length;
+}
+
+function average2(arr) {
+  let sum =0;
+   arr.forEach((element)=>{
+    if (typeof element ==='number') {
+      sum =sum + element;
+    }
+  })
+  return sum/arr.length;
+}
+
+function average3(arr) {
+  const sum = arr.reduce((acc, current)=>{
+    if (typeof current == 'number') {
+      acc = acc + current;
+    }
+    return acc;
+  }, 0);
+  return sum/arr.length;
+}
+
+console.log('Average 1 - ', average2(arr))
